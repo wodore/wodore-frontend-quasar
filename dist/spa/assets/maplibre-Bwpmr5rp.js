25d8b645aaa2b2c239e86d527e234db7868dfd1a
Tobias Burgherr <tburgherr@gmail.com>
2024-04-19 01:58:34 +0200

    Updated dist

--- dist/spa/assets/maplibre-Bwpmr5rp.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CGlk54xw.js";import{S as a}from"./vue-maplibre-gl.es-CX7g1j0x.js";const m=t(async({app:o})=>{o.use(a)});export{m as default};
---
+++ dist/spa/assets/use-quasar-CvibudSN.js
@@ -0,0 +1,1 @@
+import{i as a,aP as r}from"./index-DAawzX9R.js";function u(){return a(r)}export{u};
