13fd0ebf0c1b439e0d4e6ffb8c5adc9edfef7b44
Tobias Burgherr <tburgherr@gmail.com>
2024-05-03 19:08:17 +0200

    Updated quasar

---
+++ dist/pwa/assets/use-quasar-1QD3qNVB.js
@@ -0,0 +1,1 @@
+import{i as a,a$ as r}from"./index-BtZ0ULwG.js";function u(){return a(r)}export{u};
--- dist/pwa/assets/use-quasar-Cr0jOK5X.js
+++
@@ -1,1 +0,0 @@
-import{i as a,a$ as r}from"./index-BgdWgtZV.js";function u(){return a(r)}export{u};
