b2c72ffbe306498c903eda1f53cca2fd67930263
Tobias Burgherr <tburgherr@gmail.com>
2024-05-15 00:42:28 +0200

    Updated dist

--- dist/pwa/assets/use-quasar-C2S7eP6D.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ba as r}from"./index-BHRrNamL.js";function u(){return a(r)}export{u};
---
+++ dist/pwa/assets/use-quasar-CSCmiph_.js
@@ -0,0 +1,1 @@
+import{i as a,b8 as r}from"./index-Cgpj6ETm.js";function u(){return a(r)}export{u};
