e666f89c238e83107ee345a5d2d97fc69a4fd962
Tobias Burgherr <tburgherr@gmail.com>
2024-05-30 15:32:50 +0200

    Added new icons

---
+++ src/components/toolbar/WdToolbarExtraButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  icon: string;
+  iconColor?: string;
+  disabled?: boolean;
+}
+withDefaults(defineProps<Props>(), {
+  iconColor: 'primary-800',
+  disabled: false,
+});
+</script>
+
+<template>
+  <q-item
+    :clickable="!disabled"
+    v-ripple
+    :style="disabled ? 'opacity : 0.5;' : ''"
+  >
+    <q-item-section avatar style="min-width: 34px; padding-right: 0">
+      <q-icon :color="iconColor" :name="icon" />
+    </q-item-section>
+    <q-item-section> <slot /></q-item-section>
+  </q-item>
+</template>
