97992854089003d6da87c5e14c2a53cb0449b23d
Tobias Burgherr <tburgherr@gmail.com>
2024-06-09 22:53:20 +0200

    Updated calender select

---
+++ dist/pwa/assets/analytics-hj9MTZ8Z.js
@@ -0,0 +1,1 @@
+function o(a,i){if(typeof umami<"u")console.debug('track data: "'+a+'"',i),umami.track(a,i);else{console.warn("Umami is not available.");return}}export{o as t};
---
+++ dist/pwa/assets/use-quasar-CFE8JJCh.js
@@ -0,0 +1,1 @@
+import{q as a,bw as r}from"./index-D8u3bYpf.js";function u(){return a(r)}export{u};
