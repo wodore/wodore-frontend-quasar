32ab9c33efc5f69a8ff9e7c409b713a591976087
Tobias Burgherr <tburgherr@gmail.com>
2024-10-12 02:26:56 +0200

    Rebuild with correct api url

--- dist/pwa/assets/maplibre-C4BTx6U7.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-0v-XRemL.js";import r from"./vue-maplibre-gl.es-DrYwFPqF.js";import"./_commonjsHelpers-BosuxZz1.js";const s=t(async({app:o})=>{o.use(r)});export{s as default};
---
+++ dist/pwa/assets/use-hydration-B22f958-.js
@@ -0,0 +1,1 @@
+import{r,aN as a,z as t}from"./index-4pR3--CL.js";function u(){const e=r(!a.value);return e.value===!1&&t(()=>{e.value=!0}),{isHydrated:e}}export{u};
