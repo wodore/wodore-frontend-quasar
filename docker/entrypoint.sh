247a8d6d489cc3819b5d1baf6bea8bc6aec2fab2
Tobias Burgherr <tburgherr@gmail.com>
2026-03-07 00:09:03 +0100

    Fix dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-CONFIG=${WODORE_NGINX_CONFIG:-nginx-local.conf}
+CONFIG=${WODORE_NGINX_CONFIG:-default}
 # Map config names to actual file names
 case "$CONFIG" in
-  nginx-local.conf)
+  nginx-local.conf|local)
     CONFIG_FILE="local"
     ;;
-  nginx-proxy.conf)
+  nginx-proxy.conf|proxy)
     CONFIG_FILE="proxy"
+    ;;
+  default|nginx-default.conf|"")
+    CONFIG_FILE="default"
     ;;
   *)
     CONFIG_FILE="$CONFIG"
     ;;
 esac
-# Copy the selected config to the nginx conf directory
-cp "/etc/nginx/conf.d/10_${CONFIG_FILE}.conf.not_used" "/etc/nginx/conf.d/10_${CONFIG_FILE}.conf"
+# Copy the selected config to the nginx http.d directory
+cp "/etc/nginx/http.d/${CONFIG_FILE}.conf.not_used" "/etc/nginx/http.d/${CONFIG_FILE}.conf"
+# Remove default config if using a custom one
+if [ "$CONFIG_FILE" != "default" ]; then
+  rm -f "/etc/nginx/http.d/default.conf"
+fi
+# Create necessary directories for nginx
+mkdir -p /run/nginx
 exec /usr/local/bin/replace_vars --template /dot_env_defaults --directory /usr/share/nginx/html --patterns="*.js,*.css,*.html,*.json" --log-level ${REPLACE_VARS_LOG_LEVEL:-info} -- "$@"
