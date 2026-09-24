27e89b944b41993a53c879d1c9b577ae00e34540
TBxy <TBxy@users.noreply.github.com>
2026-09-24 16:22:43 +0000

    Preview pr-150 @ 0cfd12bac0c7b93daf2e9d93802ff4866c4ee87f 151d72896d93f3a6ed39cbac35571ae2643b0ab5

---
+++ pr-150/assets/auth-BJMUMw2c.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-8eSIhnr4.js";import{u as n}from"./useAuthService-2enHYaq_.js";import"./auth-store-BRHGHNfo.js";import"./___vite-browser-external_commonjs-proxy-CHR-kdpd.js";const u=o(async()=>{n().signinSilent().then().catch(t=>console.warn("Could not silent login",t))});export{u as default};
---
+++ pr-150/assets/use-quasar-DNJr6Pie.js
@@ -0,0 +1,1 @@
+import{i as a,bH as r}from"./index-8eSIhnr4.js";function u(){return a(r)}export{u};
