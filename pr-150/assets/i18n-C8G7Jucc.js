dd0f06c375d7e3d668fc04106ae774765762b660
TBxy <TBxy@users.noreply.github.com>
2026-09-25 22:39:50 +0000

    Preview pr-150 @ 62c98e3bc979ad039db6b5b5a5990e7ba481c025 224d13cbe9d1be5144483d880d40a5c9da826899

---
+++ pr-150/assets/i18n-C8G7Jucc.js
@@ -0,0 +1,1 @@
+import{b as s}from"./index-CRjxUHql.js";import{b as a,i,g as r,s as l,a as o}from"./locale-Bc5RQlaj.js";import{u as n,d as g}from"./user-settings-store-uDPvAnXV.js";import"./vue-i18n.runtime-CK57TMGW.js";const S=s(({app:t,store:e})=>{a(t.config.globalProperties.$q),n(e).hasStoredSettings?i(r(e)):l(g()),t.use(o),t.config.globalProperties.$t=o.global.t});export{S as default};
---
+++ pr-150/assets/maplibre-wh3nZZB1.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-CRjxUHql.js";import{F as r}from"./vue-maplibre-gl.es-B3bv_hW3.js";const e=a(async({app:o})=>{o.use(r)});export{e as default};
