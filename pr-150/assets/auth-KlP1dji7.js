ef8a5ca6f3c321ec87681de0203659e312b2c8fc
TBxy <TBxy@users.noreply.github.com>
2026-09-25 14:55:03 +0000

    Preview pr-150 @ d657b85628b631789231089299ea380ab788ae01 bf19d0b98898bfa34cce428ea0750ecf3a1908b4

---
+++ pr-150/assets/auth-KlP1dji7.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-eFaU6VQU.js";import{u as n}from"./useAuthService-SMVNYcN_.js";import"./auth-store-ObhgsPFZ.js";import"./___vite-browser-external_commonjs-proxy-CHR-kdpd.js";const u=o(async()=>{n().signinSilent().then().catch(t=>console.warn("Could not silent login",t))});export{u as default};
---
+++ pr-150/assets/i18n-gLfvrFmU.js
@@ -0,0 +1,1 @@
+import{b as s}from"./index-eFaU6VQU.js";import{b as a,i,g as r,s as l,a as o}from"./locale-BRgHb_p1.js";import{u as n,d as g}from"./user-settings-store-BROUothu.js";import"./vue-i18n.runtime-B1wp3OdV.js";const S=s(({app:t,store:e})=>{a(t.config.globalProperties.$q),n(e).hasStoredSettings?i(r(e)):l(g()),t.use(o),t.config.globalProperties.$t=o.global.t});export{S as default};
