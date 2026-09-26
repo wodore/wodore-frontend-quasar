48e19d6c4fd4ce1fbf2b4f44060d22b6faa7dc1f
TBxy <TBxy@users.noreply.github.com>
2026-09-26 01:37:22 +0000

    Preview pr-150 @ 03a07ae8271174e9e3a1ef88824daf7e6866474f 050900e86c5ded403962652d5f1ce7019e187cde

---
+++ pr-150/assets/auth-8-d73Ny9.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-EwbfFavf.js";import{u as n}from"./useAuthService-BRmg53s_.js";import"./auth-store-CV8Iab3e.js";import"./___vite-browser-external_commonjs-proxy-CHR-kdpd.js";const u=o(async()=>{n().signinSilent().then().catch(t=>console.warn("Could not silent login",t))});export{u as default};
---
+++ pr-150/assets/maplibre-C4MpTtae.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-EwbfFavf.js";import{F as r}from"./vue-maplibre-gl.es-Drbrm0JN.js";const e=a(async({app:o})=>{o.use(r)});export{e as default};
